8b7bb6db5f8901528bdb0238d74eb46f82d70f41
DigiBanks99 <wlboshoff@gmail.com>
2014-01-14 16:40:04 +0200

    Initial Commit

---
+++ FileSync/Base/FileConsts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FileSync
+{
+  public class FileConsts
+  {
+    public const int SUCCESS = 1;
+    public const int NOACTION = 0;
+    public const int ERROR = -1;
+  }
+}
---
+++ FileSync/Base/IFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FileSync
+{
+  public interface IFile
+  {
+    string Name { get; set; }
+    string FullName { get; set; }
+    long Size { get; set; }
+    DateTime DateCreated { get; set; }
+    string Type { get; set; }
+
+    int Compare(IFile file);
+    int Copy(IFile file, string dir);
+    int Delete(IFile file, string dir);
+
+    void PopulateFileDetails(string fName);
+  }
+}
