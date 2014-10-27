819a176cad58de4d60bd0b89cddc1658b9356a80
unknown <wilcob@VIP-Wilco.sagevip.za.adinternal.com>
2014-10-27 14:13:33 +0200

    XML Functionality for file management

---
+++ FileSync/WatchItemList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSync
+{
+  public class WatchItemList : BaseList<WatchItem>
+  {
+    #region Constructor
+    public WatchItemList()
+    {
+
+    }
+    #endregion Constructor
+
+    #region Overrides
+    public virtual WatchItem AddNew()
+    {
+      var watchItem = new WatchItem(true);
+      this.Add(watchItem);
+      return watchItem;
+    }
+    #endregion Overrides
+
+    #region IBaseList
+    public WatchItemList NewList()
+    {
+      return new WatchItemList();
+    }
+    #endregion IBaseList
+
+    #region Public Methods
+    public WatchItemList GetFromXml(string path)
+    {
+      var list = NewList();
+      if (path == null || path.Length < 1)
+        return list;
+
+      using (var stream = new System.IO.MemoryStream())
+      {
+        using (var xmlReader = System.Xml.XmlReader.Create(stream))
+        {
+          XMLManager.ReadFromXml(xmlReader, list);
+        }
+      }
+
+      return list;
+    }
+    #endregion Public Methods
+  }
+}
